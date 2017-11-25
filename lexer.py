8c4f4e68801cc9e5e1e88b296205028ee3507193
GeovanniOj <geovannirock@gmail.com>
2017-11-25 17:36:37 -0200

    creation of the lexer

--- lexer.py
+++ lexer.py
@@ -1,16 +1,15 @@
 
 import ox 
 
-tokens = {
-    "right" : ">",
-    "left" : "<",
-    "inc" : "+" ,
-    "dec" : "-",
-    "print" : ".",
-    "read" : ",",
-    "(loop":"[",
-    ")" : "]"      
-}
+lexer = [
+    ('PARANTHESIS_OPENED', r'\('),
+    ('PARANTHESIS_CLOSED', r'\)'),
+    ('PLAIN_TEXT', r'[-a-zA-Z]+'),
+    ('NUMBERS', r'\d+'),
+    ('NEWLINE', r'\s+'),
+    ('COMMENT', r';[^\n]*'),
+]
+
 
 #def parser():
 
