6037e7f47b5222cb37953d7b580b582708cac02f
GeovanniOj <geovannirock@gmail.com>
2017-11-26 20:19:19 -0200

    parser was made and tokens from the created lexer have been create

--- lexer.py
+++ lexer.py
@@ -1,15 +1,33 @@
 
 import ox 
 
-lexer = [
+lexer = ox.make_lexer([
     ('PARANTHESIS_OPENED', r'\('),
     ('PARANTHESIS_CLOSED', r'\)'),
     ('PLAIN_TEXT', r'[-a-zA-Z]+'),
     ('NUMBERS', r'\d+'),
     ('NEWLINE', r'\s+'),
-    ('COMMENT', r';[^\n]*'),
+    ('COMMENT', r';[^\n]*')
+])
+
+tokens = [
+    'PARANTHESIS_OPENED',
+    'PARANTHESIS_CLOSED',
+    'PLAIN_TEXT',
+    'NUMBERS',
+    'NEWLINE',
+    'COMMENT'
 ]
 
+parser = ox.make_parser([
+    ('atom : PLAIN_TEXT', lambda x: x),
+    ('atom : NUMBERS', lambda x: X),
+    ('block : PARANTHESIS_OPENED atom PARANTHESIS_CLOSED', lambda x, y, z: y),
+    ('block : PARANTHESIS_OPENED PARANTHESIS_CLOSED'lambda x,y:'()'),
+    ('exp : atom exp', lambda x, y: (x,) + y),
+    ('exp : atom', lambda x: (x,)),
+    ('atom : block', lambda x: x),   
+], tokens)
 
 #def parser():
 
