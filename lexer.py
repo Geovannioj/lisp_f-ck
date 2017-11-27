ff94038d3c9cce78e35ddddfb6b772545c07e54c
GeovanniOj <geovannirock@gmail.com>
2017-11-27 09:21:07 -0200

    function to print the syntatic tree and lisp code introduced to the exemple.lf file

--- lexer.py
+++ lexer.py
@@ -1,33 +1,47 @@
 
-import ox 
+import ox
+import click
+import pprint
+
+
 
 lexer = ox.make_lexer([
     ('PARANTHESIS_OPENED', r'\('),
     ('PARANTHESIS_CLOSED', r'\)'),
     ('PLAIN_TEXT', r'[-a-zA-Z]+'),
     ('NUMBERS', r'\d+'),
-    ('NEWLINE', r'\s+'),
-    ('COMMENT', r';[^\n]*')
+    ('ignore_NEWLINE', r'\s+'),
+    ('ignore_COMMENT', r';[^\n]*'),
 ])
+
 
 tokens = [
     'PARANTHESIS_OPENED',
     'PARANTHESIS_CLOSED',
     'PLAIN_TEXT',
     'NUMBERS',
-    'NEWLINE',
-    'COMMENT'
+    'ignore_NEWLINE',
+    'ignore_COMMENT',
 ]
 
 parser = ox.make_parser([
     ('atom : PLAIN_TEXT', lambda x: x),
-    ('atom : NUMBERS', lambda x: X),
-    ('block : PARANTHESIS_OPENED atom PARANTHESIS_CLOSED', lambda x, y, z: y),
-    ('block : PARANTHESIS_OPENED PARANTHESIS_CLOSED'lambda x,y:'()'),
-    ('exp : atom exp', lambda x, y: (x,) + y),
-    ('exp : atom', lambda x: (x,)),
-    ('atom : block', lambda x: x),   
+    ('atom : NUMBERS', lambda x: x),
+    ('expr : atom expr', lambda x, y: (x,) + y),
+    ('expr : atom', lambda x: (x,)),
+    ('block : PARANTHESIS_OPENED PARANTHESIS_CLOSED', lambda x, y: '()'),
+    ('block : PARANTHESIS_OPENED expr PARANTHESIS_CLOSED', lambda x, y, z: y),
+    ('atom : block', lambda x: x),
 ], tokens)
 
-#def parser():
+
+
+@click.command()
+@click.argument('lisp_file', type=click.File('r'))
+def printTree(lisp_file):
+    
+    tokens = lexer(lisp_file.read())
+    pprint.pprint(parser(tokens))
+
+printTree()
 
